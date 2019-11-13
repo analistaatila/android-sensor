ad6480f329a91cbec9162c2e379103a300d73212
Macbook pro retina <devarthurribeiro@gmail.com>
2019-11-13 09:20:48 -0300

    Acelerometro e giroscopio

---
+++ app/src/main/java/com/tads/sensor/GyroscopeActivity.kt
@@ -0,0 +1,36 @@
+package com.tads.sensor
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_gyroscope.*
+class GyroscopeActivity : AppCompatActivity(), SensorEventListener {
+
+    lateinit var sensorManager: SensorManager
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        sensorData.text = "GIROSCOPIO \n\n X = ${event!!.values[0]}\n\n Y = ${event?.values[1]} \n\n Z = ${event?.values?.get(2)}"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gyroscope)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        sensorManager.registerListener(
+            this,
+            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+}
+
--- app/src/main/java/com/tads/sensor/MainActivity.kt
+++ app/src/main/java/com/tads/sensor/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.tads.sensor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        acBtn.setOnClickListener {
+            startActivity(Intent(this, AcelerometerActivity::class.java))
+        }
+
+        gyBtn.setOnClickListener {
+            startActivity(Intent(this, GyroscopeActivity::class.java))
+        }
     }
 }
